7593ff0dc78ac46b6b9e1d2fcfb2d3962000a481
Ba Thien Le <bathien.le.pro@gmail.com>
2024-08-19 16:51:38 +0200

    test: add index annotation test

--- src/test/java/be/cytomine/service/search/RetrievalServiceTests.java
+++ src/test/java/be/cytomine/service/search/RetrievalServiceTests.java
@@ -1,7 +1,13 @@
 package be.cytomine.service.search;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
+import com.vividsolutions.jts.io.ParseException;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -12,10 +18,11 @@ import org.springframework.http.ResponseEntity;
 
 import be.cytomine.BasicInstanceBuilder;
 import be.cytomine.CytomineCoreApplication;
+import be.cytomine.api.controller.ontology.UserAnnotationResourceTests;
 import be.cytomine.domain.ontology.UserAnnotation;
+import be.cytomine.service.dto.CropParameter;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(classes = CytomineCoreApplication.class)
@@ -31,14 +38,59 @@ public class RetrievalServiceTests {
 
     @BeforeAll
     public static void beforeAll() {
-        wireMockServer = new WireMockServer(9999);
+        wireMockServer = new WireMockServer(8888);
         wireMockServer.start();
-        WireMock.configureFor("localhost", 9999);
+        WireMock.configureFor("localhost", 8888);
     }
 
     @AfterAll
     public static void afterAll() {
         wireMockServer.stop();
+    }
+
+    @Test
+    public void index_annotation_with_success() throws ParseException, UnsupportedEncodingException {
+        UserAnnotation annotation = UserAnnotationResourceTests.given_a_user_annotation_with_valid_image_server(builder);
+
+        String id = URLEncoder.encode(annotation.getSlice().getBaseSlice().getPath(), StandardCharsets.UTF_8);
+        String url = "/image/" + id + "/annotation/crop";
+        String body = "{\"annotations\":{},\"level\":0,\"background_transparency\":0,\"z_slices\":0,\"timepoints\":0}";
+
+        wireMockServer.stubFor(WireMock.post(urlEqualTo(url))
+            .withRequestBody(
+                WireMock.equalToJson(body)
+            )
+            .willReturn(aResponse()
+                .withStatus(HttpStatus.OK.value())
+                .withBody(UUID.randomUUID().toString().getBytes())
+            )
+        );
+
+        String expectedUrlPath = "/api/images";
+        String expectedResponseBody = "{ \"ids\": [" + annotation.getId() + "]";
+        expectedResponseBody += ", \"storage\": " + annotation.getProject().getId().toString();
+        expectedResponseBody += ", \"index\": \"annotation\" }";
+
+        wireMockServer.stubFor(WireMock.post(urlPathEqualTo(expectedUrlPath))
+            .withQueryParam("storage", WireMock.equalTo(annotation.getProject().getId().toString()))
+            .withQueryParam("index", WireMock.equalTo("annotation"))
+            .willReturn(aResponse()
+                .withStatus(HttpStatus.OK.value())
+                .withHeader("Content-Type", "application/json")
+                .withBody(expectedResponseBody)
+            )
+        );
+
+        CropParameter parameters = new CropParameter();
+        parameters.setFormat("png");
+        ResponseEntity<String> response = retrievalService.indexAnnotation(annotation, parameters, "");
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(expectedResponseBody, response.getBody());
+
+        wireMockServer.verify(WireMock.postRequestedFor(urlPathEqualTo(expectedUrlPath))
+            .withQueryParam("storage", WireMock.equalTo(annotation.getProject().getId().toString()))
+            .withQueryParam("index", WireMock.equalTo("annotation")));
     }
 
     @Test
