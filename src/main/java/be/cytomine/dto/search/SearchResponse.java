5e60164ecb072c21716a41d5f0123052a7104690
Ba Thien Le <bathien.le.pro@gmail.com>
2024-08-20 10:17:04 +0200

    feat: add all args constructor annotation

--- src/main/java/be/cytomine/dto/search/SearchResponse.java
+++ src/main/java/be/cytomine/dto/search/SearchResponse.java
@@ -2,10 +2,12 @@ package be.cytomine.dto.search;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class SearchResponse {
 
