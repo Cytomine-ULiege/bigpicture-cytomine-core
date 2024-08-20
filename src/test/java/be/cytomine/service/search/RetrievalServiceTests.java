eca2d8c8e731fc37daef25d5770dec7d0b8c3ec1
Ba Thien Le <bathien.le.pro@gmail.com>
2024-08-20 10:15:20 +0200

    test: add test for retrieveSimilarImages()

--- src/test/java/be/cytomine/service/search/RetrievalServiceTests.java
+++ src/test/java/be/cytomine/service/search/RetrievalServiceTests.java
@@ -3,8 +3,11 @@ package be.cytomine.service.search;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.UUID;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.vividsolutions.jts.io.ParseException;
@@ -20,6 +23,7 @@ import be.cytomine.BasicInstanceBuilder;
 import be.cytomine.CytomineCoreApplication;
 import be.cytomine.api.controller.ontology.UserAnnotationResourceTests;
 import be.cytomine.domain.ontology.UserAnnotation;
+import be.cytomine.dto.search.SearchResponse;
 import be.cytomine.service.dto.CropParameter;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
@@ -52,6 +56,7 @@ public class RetrievalServiceTests {
     public void index_annotation_with_success() throws ParseException, UnsupportedEncodingException {
         UserAnnotation annotation = UserAnnotationResourceTests.given_a_user_annotation_with_valid_image_server(builder);
 
+        /* Simulate call to PIMS */
         String id = URLEncoder.encode(annotation.getSlice().getBaseSlice().getPath(), StandardCharsets.UTF_8);
         String url = "/image/" + id + "/annotation/crop";
         String body = "{\"annotations\":{},\"level\":0,\"background_transparency\":0,\"z_slices\":0,\"timepoints\":0}";
@@ -66,6 +71,7 @@ public class RetrievalServiceTests {
             )
         );
 
+        /* Simulate call to CBIR */
         String expectedUrlPath = "/api/images";
         String expectedResponseBody = "{ \"ids\": [" + annotation.getId() + "]";
         expectedResponseBody += ", \"storage\": " + annotation.getProject().getId().toString();
@@ -81,6 +87,7 @@ public class RetrievalServiceTests {
             )
         );
 
+        /* Test index annotation method */
         CropParameter parameters = new CropParameter();
         parameters.setFormat("png");
         ResponseEntity<String> response = retrievalService.indexAnnotation(annotation, parameters, "");
@@ -97,6 +104,7 @@ public class RetrievalServiceTests {
     public void delete_index_with_success() {
         UserAnnotation annotation = builder.given_a_user_annotation();
 
+        /* Simulate call to CBIR */
         String expectedUrlPath = "/api/images/" + annotation.getId();
         String expectedResponseBody = "{ \"id\": " + annotation.getId();
         expectedResponseBody += ", \"storage\": " + annotation.getProject().getId().toString();
@@ -112,6 +120,7 @@ public class RetrievalServiceTests {
             )
         );
 
+        /* Test delete index method */
         ResponseEntity<String> response = retrievalService.deleteIndex(annotation);
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
@@ -120,5 +129,73 @@ public class RetrievalServiceTests {
         wireMockServer.verify(WireMock.deleteRequestedFor(urlPathEqualTo(expectedUrlPath))
             .withQueryParam("storage", WireMock.equalTo(annotation.getProject().getId().toString()))
             .withQueryParam("index", WireMock.equalTo("annotation")));
+    }
+
+    @Test
+    public void search_similar_images_with_success() throws JsonProcessingException, ParseException,
+        UnsupportedEncodingException {
+        UserAnnotation annotation =
+            UserAnnotationResourceTests.given_a_user_annotation_with_valid_image_server(builder);
+
+        /* Simulate call to PIMS */
+        String id = URLEncoder.encode(annotation.getSlice().getBaseSlice().getPath(), StandardCharsets.UTF_8);
+        String url = "/image/" + id + "/annotation/crop";
+        String body = "{\"annotations\":{},\"level\":0,\"background_transparency\":0,\"z_slices\":0,\"timepoints\":0}";
+
+        wireMockServer.stubFor(WireMock.post(urlEqualTo(url))
+            .withRequestBody(
+                WireMock.equalToJson(body)
+            )
+            .willReturn(aResponse()
+                .withStatus(HttpStatus.OK.value())
+                .withBody(UUID.randomUUID().toString().getBytes())
+            )
+        );
+
+        /* Simulate call to CBIR */
+        ObjectMapper objectMapper = new ObjectMapper();
+        String expectedUrlPath = "/api/search";
+        SearchResponse expectedResponse = new SearchResponse(
+            annotation.getId().toString(),
+            annotation.getProject().getId().toString(),
+            "annotation",
+            Arrays.asList(
+                Arrays.asList(annotation.getId().toString(), 0.0),
+                Arrays.asList("1", 123.0),
+                Arrays.asList("2", 456.0)
+            )
+        );
+
+        wireMockServer.stubFor(WireMock.post(urlPathEqualTo(expectedUrlPath))
+            .withQueryParam("storage", WireMock.equalTo(annotation.getProject().getId().toString()))
+            .withQueryParam("index", WireMock.equalTo("annotation"))
+            .willReturn(aResponse()
+                .withStatus(HttpStatus.OK.value())
+                .withHeader("Content-Type", "application/json")
+                .withBody(objectMapper.writeValueAsString(expectedResponse))
+            )
+        );
+
+        /* Test retrieve similar images method */
+        Long neighbours = 2L;
+        CropParameter parameters = new CropParameter();
+        parameters.setFormat("png");
+
+        ResponseEntity<SearchResponse> response = retrievalService.retrieveSimilarImages(
+            annotation,
+            parameters,
+            "",
+            neighbours
+        );
+
+        expectedResponse.setSimilarities(
+            Arrays.asList(
+                Arrays.asList("1", 73.02631578947368),
+                Arrays.asList("2", 0.0)
+            )
+        );
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(expectedResponse, response.getBody());
     }
 }
