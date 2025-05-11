95af64ae380570a1f0d1558afc8660f39e048088
Ba Thien LE <bathien.le.pro@gmail.com>
2025-05-11 16:48:48 +0200

    refactor: rename to RetrievalController

--- src/main/java/be/cytomine/controller/search/RetrievalController.java
+++ src/main/java/be/cytomine/controller/search/RetrievalController.java
@@ -1,25 +1,8 @@
-package be.cytomine.api.controller.search;
-
-/*
- * Copyright (c) 2009-2023. Authors: see NOTICE file.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package be.cytomine.controller.search;
 
 import java.io.UnsupportedEncodingException;
-import javax.persistence.EntityManager;
 
-import com.vividsolutions.jts.io.ParseException;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -28,17 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import be.cytomine.api.controller.RestCytomineController;
 import be.cytomine.domain.ontology.AnnotationDomain;
+import be.cytomine.dto.image.CropParameter;
 import be.cytomine.dto.search.SearchResponse;
-import be.cytomine.service.dto.CropParameter;
 import be.cytomine.service.search.RetrievalService;
 
 @Slf4j
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
-public class RestRetrievalController extends RestCytomineController {
+public class RetrievalController {
 
     private final EntityManager entityManager;
 
